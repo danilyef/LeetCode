5a17af72744c169ca169d3b5f3365b6a87bc0561
danilyef <daniil.yefimov92@gmail.com>
2025-02-09 21:01:50 +0100

    Contain Duplicates

---
+++ easy/containsDuplicates.py
@@ -0,0 +1,48 @@
+'''
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Explanation:
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+
+Explanation:
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+'''
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(set(nums)) < len(nums):
+            return True
+        return False
+    
+
+
+class Solution:
+    def containsDuplicate(self, nums):
+        elements_seen = set()
+        for num in nums:
+            if num in elements_seen:
+                return True
+            elements_seen.add(num)
+        return False
+
+
